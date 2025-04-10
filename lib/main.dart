4e7aebd78cda007869b46ca40db51f0d42ae2df7
rodrigo0369 <rodrigoirala18@gmail.com>
2025-04-10 08:49:06 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,64 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'screens/home_screen.dart';
-import 'utils/language_constants.dart';
-import 'services/notification_service.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'l10n/l10n.dart';
+import 'services/notifications_service.dart';
+import 'screens/home_screen.dart';
+import 'screens/glucose_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await NotificationService().init(); // Inicializar notificaciones
+  await NotificationsService.init();
   runApp(MyApp());
 }
 
 class MyApp extends StatefulWidget {
   @override
-  State<MyApp> createState() => _MyAppState();
+  _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
   Locale? _locale;
 
   @override
-  void didChangeDependencies() {
-    getLocale().then((locale) {
-      setState(() {
-        _locale = locale;
-      });
+  void initState() {
+    super.initState();
+    _loadLocale();
+  }
+
+  Future<void> _loadLocale() async {
+    final prefs = await SharedPreferences.getInstance();
+    final langCode = prefs.getString('language_code') ?? 'es';
+    setState(() {
+      _locale = Locale(langCode);
     });
-    super.didChangeDependencies();
   }
 
   @override
   Widget build(BuildContext context) {
     if (_locale == null) {
-      return const CircularProgressIndicator();
-    } else {
-      return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'DiabetesHabitsApp',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        locale: _locale,
-        supportedLocales: const [
-          Locale('es', ''),
-          Locale('en', ''),
-        ],
-        localizationsDelegates: const [
-          GlobalMaterialLocalizations.delegate,
-          GlobalWidgetsLocalizations.delegate,
-          GlobalCupertinoLocalizations.delegate,
-        ],
-        home: HomeScreen(),
-      );
+      return CircularProgressIndicator(); // Mientras carga el idioma
     }
+
+    return MaterialApp(
+      title: 'Diabetes Habits',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      locale: _locale,
+      supportedLocales: L10n.all,
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomeScreen(),
+        '/glucose': (context) => GlucoseScreen(),
+      },
+    );
   }
 }
