9a3dde76b9381de80c9763da9ccdf7756059d838
rodrigo0369 <rodrigoirala18@gmail.com>
2025-04-10 09:57:31 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,64 +1,42 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'l10n/l10n.dart';
-import 'services/notifications_service.dart';
-import 'screens/home_screen.dart';
-import 'screens/glucose_screen.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:diabetes_habits_app/screens/home_screen.dart';
+import 'package:diabetes_habits_app/screens/glucose_screen.dart';
+import 'package:diabetes_habits_app/screens/habits_screen.dart';
+import 'package:diabetes_habits_app/screens/settings_screen.dart';
+import 'package:diabetes_habits_app/screens/country_selection_screen.dart';
+import 'package:diabetes_habits_app/screens/diabetes_type_screen.dart';
+import 'package:diabetes_habits_app/screens/recommendations_screen.dart';
+import 'package:diabetes_habits_app/services/notification_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await NotificationsService.init();
-  runApp(MyApp());
+  await NotificationService().init(); // Inicializar notificaciones
+  runApp(const DiabetesHabitsApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  Locale? _locale;
-
-  @override
-  void initState() {
-    super.initState();
-    _loadLocale();
-  }
-
-  Future<void> _loadLocale() async {
-    final prefs = await SharedPreferences.getInstance();
-    final langCode = prefs.getString('language_code') ?? 'es';
-    setState(() {
-      _locale = Locale(langCode);
-    });
-  }
+class DiabetesHabitsApp extends StatelessWidget {
+  const DiabetesHabitsApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    if (_locale == null) {
-      return CircularProgressIndicator(); // Mientras carga el idioma
-    }
-
     return MaterialApp(
-      title: 'Diabetes Habits',
-      debugShowCheckedModeBanner: false,
+      title: 'Diabetes Habits App',
       theme: ThemeData(
         primarySwatch: Colors.teal,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      locale: _locale,
-      supportedLocales: L10n.all,
-      localizationsDelegates: [
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
       initialRoute: '/',
       routes: {
-        '/': (context) => HomeScreen(),
-        '/glucose': (context) => GlucoseScreen(),
+        '/': (context) => const HomeScreen(),
+        '/glucose': (context) => const GlucoseScreen(),
+        '/habits': (context) => const HabitsScreen(),
+        '/settings': (context) => const SettingsScreen(),
+        '/country': (context) => const CountrySelectionScreen(),
+        '/diabetes': (context) => const DiabetesTypeScreen(),
+        '/recommendations': (context) => const RecommendationsScreen(),
       },
+      debugShowCheckedModeBanner: false,
     );
   }
 }
